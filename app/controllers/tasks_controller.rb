9cf870f3c0120b61aa3d10fc21e3121bd74b0f06
Diogo <drd.rclrd@gmail.com>
2019-09-25 13:49:17 -0300

    added create and save

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,20 @@
 class TasksController < ApplicationController
   def index
-    @tasks = []
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params) 
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end

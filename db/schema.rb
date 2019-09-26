9454f2b1b09cc154d1d51ca2401e68511bd37ecc
Diogo <drd.rclrd@gmail.com>
2019-09-26 17:36:42 -0300

    added column completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_133939) do
+ActiveRecord::Schema.define(version: 2019_09_26_203116) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end

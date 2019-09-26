0349f114d9b6c4d224c681007d0ef916f8bf5f5a
Diogo <drd.rclrd@gmail.com>
2019-09-26 18:40:53 -0300

    added param completed when update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,6 +41,6 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end

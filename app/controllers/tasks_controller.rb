515b5bf7f7943d80f9c5f04cea9537ca01c47946
Diogo <drd.rclrd@gmail.com>
2019-09-26 14:19:38 -0300

    create define destroy task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,12 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :details)

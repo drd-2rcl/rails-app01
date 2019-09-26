86a36be73bf433e0bc064643418f7bd5d3b8e584
Diogo <drd.rclrd@gmail.com>
2019-09-26 17:26:36 -0300

    created conditional for render and redirect

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,8 +13,11 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params) 
-    @task.save
-    redirect_to tasks_path
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,8 +26,11 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(task_params)
-    redirect_to tasks_path
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy

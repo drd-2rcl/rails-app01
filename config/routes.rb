a98de89bd13c16049465d6e9f9b0353b1a4fa0ef
Diogo <drd.rclrd@gmail.com>
2019-09-26 23:12:59 -0300

    added new route for complete task

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   resources :tasks
+
+  get 'tasks/:id/complete/:completed', to: 'tasks#complete'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
